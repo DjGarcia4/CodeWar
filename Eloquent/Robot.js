dee6608553aadd9cf4b3a1bd0c69194c693b6d3f
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2024-08-21 10:20:15 -0600

    Eloquent Robot project

---
+++ Eloquent/Robot.js
@@ -0,0 +1,119 @@
+const caminos = [
+  "Casa de Alicia-Casa de Bob",
+  "Casa de Alicia-Oficina de Correos",
+  "Casa de Daria-Casa de Ernie",
+  "Casa de Ernie-Casa de Grete",
+  "Casa de Grete-Tienda",
+  "Mercado-Oficina de Correos",
+  "Mercado-Ayuntamiento",
+  "Casa de Alicia-Cabaña",
+  "Casa de Bob-Ayuntamiento",
+  "Casa de Daria-Ayuntamiento",
+  "Casa de Grete-Granja",
+  "Mercado-Granja",
+  "Mercado-Tienda",
+  "Tienda-Ayuntamiento",
+];
+
+const rutaCorreo = [
+  "Casa de Alicia",
+  "Cabaña",
+  "Casa de Alicia",
+  "Casa de Bob",
+  "Ayuntamiento",
+  "Casa de Daria",
+  "Casa de Ernie",
+  "GCasa de Grete",
+  "Tienda",
+  "Casa de Grete",
+  "Granja",
+  "Mercado",
+  "Oficina de Correos",
+];
+
+function construirGrafo(bordes) {
+  let grafo = Object.create(null);
+  function añadirBorde(desde, hasta) {
+    if (grafo[desde] == null) {
+      grafo[desde] = [hasta];
+    } else {
+      grafo[desde].push(hasta);
+    }
+  }
+  for (let [desde, hasta] of bordes.map((c) => c.split("-"))) {
+    añadirBorde(desde, hasta);
+    añadirBorde(hasta, desde);
+  }
+  return grafo;
+}
+
+class EstadoPueblo {
+  constructor(lugar, paquetes) {
+    this.lugar = lugar;
+    this.paquetes = paquetes;
+  }
+
+  mover(destino) {
+    if (!grafoCamino[this.lugar].includes(destino)) {
+      return this;
+    } else {
+      let paquete = this.paquetes
+        .map((p) => {
+          if (p.lugar != this.lugar) return p;
+          return { lugar: destino, direccion: p.direccion };
+        })
+        .filter((p) => p.lugar != direccion);
+      return new EstadoPueblo(destino, paquete);
+    }
+  }
+}
+
+let primero = new EstadoPueblo("Oficina de Correos", [
+  { lugar: "Oficina de Correos", direccion: "Cassa de Alicia" },
+]);
+
+let siguiente = primero.mover("Casa de Alicia");
+
+const grafoCamino = construirGrafo(roads);
+
+function correrRobot(estado, robot, memoria) {
+  for (let turno = 0; ; turno++) {
+    if (estado.paquetes.length == 0) {
+      console.log(`Listo en ${turno} turnos`);
+      break;
+    }
+    let accion = robot(estado, memoria);
+    estado = estado.mover(accion.direccion);
+    memoria = accion.memoria;
+    console.log(`Moverse ${accion.direccion}`);
+  }
+}
+
+function eleccionAleatoria(array) {
+  let eleccion = Math.floor(Math.random() * array.length);
+  return array[eleccion];
+}
+
+function robotAleatorio(estado) {
+  return { direccion: eleccionAleatoria(grafoCamino[estado.lugar]) };
+}
+
+EstadoPueblo.aleatorio = function (numeroDePaquetes = 5) {
+  let paquetes = [];
+  for (let i = 0; i < numeroDePaquetes; i++) {
+    let direccion = eleccionAleatoria(Object.keys(grafoCamino));
+    let lugar;
+    do {
+      lugar = eleccionAleatoria(Object.keys(grafoCamino));
+    } while (lugar == direccion);
+    paquetes.push({ lugar, direccion });
+  }
+  return new EstadoPueblo("Oficina de Correos", paquetes);
+};
+
+function robotRuta(estado, memoria) {
+  if (memoria.length == 0) {
+    memoria = rutaCorreo;
+  }
+  return { direction: memoria[0], memoria: memoria.slice(1) };
+}
