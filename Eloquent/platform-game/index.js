ddcb11575878101b2360fec119147670267b59d2
Jared Garcia <jaredgarcia@MacBook-Pro-de-Jared.local>
2024-10-23 08:42:50 -0600

    Eloquent PlatformGame DOM2

--- Eloquent/platform-game/index.js
+++ Eloquent/platform-game/index.js
@@ -178,7 +178,7 @@ function drawGrid(level) {
   );
 }
 
-function drawActor(actors) {
+function drawActors(actors) {
   return elt(
     "div",
     {},
@@ -192,3 +192,37 @@ function drawActor(actors) {
     })
   );
 }
+
+DOMDisplay.prototype.syncState = function (state) {
+  if (this.actorLayer) this.actorLayer.remove();
+  this.actorLayer = drawActors(state.actors);
+  this.dom.appendChild(this.actorLayer);
+  this.dom.className = `game ${state.statues}`;
+  this.scrollPlayerIntoView(state);
+};
+
+DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
+  let width = this.dom.clientWidth;
+  let height = this.dom.clientHeight;
+  let margin = width / 3;
+
+  //The viewport
+  let left = this.dom.scrollLeft,
+    right = left + width;
+  let top = this.dom.scrollTop,
+    bottom = top + height;
+
+  let player = state.player;
+  let center = player.pos.plus(player.size.times(0.5)).times(scale);
+
+  if (center.x < left + margin) {
+    this.dom.scrollLeft = center.x - margin;
+  } else if (center.x > right - margin) {
+    this.dom.scrollLeft = center.x + margin - width;
+  }
+  if (center.y < top + margin) {
+    this.dom.scrollTop = center.y - margin;
+  } else if (center.y > bottom - margin) {
+    this.dom.scrollTop = center.y + margin - height;
+  }
+};
